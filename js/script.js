b66be197f25e3d116efcae0f5521847397b501ab
Alberto Zordan <zorberto@gmail.com>
2020-07-30 18:23:02 +0200

    Overall chat program done

--- js/script.js
+++ js/script.js
@@ -1,71 +1,85 @@
 $(document).ready(function() {
+    // 1. Reset del campo input
     $('input').val('');
 
+    // 2. A qualsiasi input dell'utente metto in attesa il programma, che fa visualizzare il tasto 'send' solo quando vi è un contenuto (spazi bianchi compresi), altrimenti mostra il tasto 'mic'
+    $('#text-input').on('input',function() {
+        if ($(this).val()) {
+            $('#send-btn').show();
+            $('#mic-btn').hide();
+        } else {
+            $('#send-btn').hide();
+            $('#mic-btn').show();
+        }
+    });
+
+    // 3. Premendo 'invio' invoco la funzione 'chat'
     $('#text-input').keydown(function() {
         if (event.which == 13) {
             chat();
         }
     });
-    $('#mic').on('click',chat);
 
-    function chat() {
-        var testo = $('#text-input').val();
-        $('#text-input').val('');
-        if (testo.trim().length > 0) {
-            var copia = $('.template .message').clone();
-            var data = new Date();
-            var minCorrente = data.getMinutes();
-            if (minCorrente < 10) {
-                minCorrente = '0' + minCorrente;
-            }
-            var tempo = data.getHours() + ':' + minCorrente;
-            copia.children('.message-text').append(testo);
-            copia.children('.message-time').append(tempo);
-            copia.addClass('sent');
-            $('#main').append(copia);
-            $('#main').animate({scrollTop: $(copia).offset().top},0);
-            // c'è anche position() unito poi a scrollTop e top
-            setTimeout(function() {
-                var frasiRandom = [
-                    'Ciao!',
-                    'Ehi! Dimmi',
-                    'Bonjour',
-                    'No dai...',
-                    'Che ti posso dire... sono d\'accordo'
-                ];
-                var copia = $('.template .message').clone();
-                copia.children('.message-text').append(frasiRandom[numRandom(0,frasiRandom.length - 1)]);
-                copia.children('.message-time').append(tempo);
-                copia.addClass('received');
-                $('#main').append(copia);
-                $('#main').animate({scrollTop: $(copia).offset().top},0);
-            },1000);
-        }
-    }
+    // 4. Cliccando sul tasto 'send' invoco la funzione 'chat'
+    $('#send-btn').on('click',chat);
 });
 
+// Variabili globali
+var frasiRandom = [
+    'Ciao!',
+    'Ehi! Dimmi',
+    'Bonjour',
+    'No dai...',
+    'Che ti posso dire... sono d\'accordo'
+];
+
+// *** FUNZIONI *** //
+// a. Stampa sul DOM dei messaggi
+function chat() {
+    var testo = $('#text-input').val();
+    // La funzione si applica soltanto se sono stati digitati dei caratteri
+    if (testo.trim()) {
+        $('#text-input').val('');
+        // Invoco la funzione 'invia' per generare un messaggio inviato e cambio la visualizzazione del tasto 'send' con 'mic'
+        invia(testo,'sent');
+        $('#send-btn').hide();
+        $('#mic-btn').show();
+        // Dopo 1 secondo la invoco nuovamente (con gli argomenti specifici per generare un messaggio ricevuto)
+        setTimeout(invia,1000,frasiRandom[numRandom(0,frasiRandom.length - 1)],'received');
+        //
+    }
+}
+
+// b. Gestione della generazione dei messaggi col metodo .clone() e dell'orario corrente, con successivo aggiornamento della scrollbar
+function invia(messaggio,classe) {
+    // Clonazione del template
+    var copia = $('.template .message').clone();
+    // Generazione orario corrente
+    var data = new Date();
+    var minCorrente = data.getMinutes();
+    if (minCorrente < 10) {
+        minCorrente = '0' + minCorrente;
+    }
+    var orario = data.getHours() + ':' + minCorrente;
+    // Incollo messaggio e ora al clone
+    copia.find('.message-text').append(messaggio); // al posto di find posso usare children ma in un'ottica di programma scalabile è meglio find
+    copia.find('.message-time').append(orario);
+    // Attribuzione della specifica classe in base alla natura del messaggio
+    if (classe == 'sent') {
+        copia.addClass('sent');
+    } else {
+        copia.addClass('received');
+    }
+    // Incollo il clone al DOM
+    $('#main').append(copia);
+    // Aggiornamento scrollbar
+    $('#main').animate({scrollTop: $(copia).offset().top},0);
+}
+
+// c. Generazione numeri random tra min e max compresi
 function numRandom(min,max) {
     return Math.floor(Math.random()*(max - min + 1) + min);
 }
-
-    // function rispRandom() {
-    //     var frasiRandom = [
-    //         'Ciao!',
-    //         'Ehi! Dimmi',
-    //         'Bonjour',
-    //         'No dai...',
-    //         'Che ti posso dire... sono d\'accordo'
-    //     ];
-    //     copia.children('.message-text').append(frasiRandom[numRandom(0,frasiRandom.length - 1)]);
-    //     copia.children('.message-time').append(tempo);
-    //     copia.addClass('received');
-    //     $('#main').append(copia);
-    // }
-
-
-// $('#text-input').on('input',function() {
-    //     $('#mic').hide();
-    // });
 
 // $(document).ready(function() {
 //     $("button").click(function() {
