7040371534edfe6c6a63b411c027f56d5dd2eb84
Alberto Zordan <zorberto@gmail.com>
2020-08-21 18:03:54 +0200

    Search contact function done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    // ! 1. Reset del campo input e riposizionamento scrollbar dei contatti
+    // ! 1. Reset del campo input
     $('input').val('');
-    $('#contacts-section').animate({scrollTop: 0},0);
 
-    // ! 2. Al click su un contatto a sx lo evidenzio e visualizzo la chat a esso associata, con aggiornamento del nome del contatto e dell'orario di ultimo accesso
+    // ! 2. Al click su un contatto a sx lo evidenzio e visualizzo la chat a esso associata, con aggiornamento di immagine e nome del contatto e dell'orario di ultimo accesso
     $('.contact-box').click(function() {
         $('#wa-start').hide();
         $('#chat').show();
@@ -36,16 +35,21 @@ $(document).ready(function() {
 
     // ! 5. Cliccando sul tasto 'send' invoco la funzione 'chat'
     $('#send-btn').on('click',chat);
+
+    // ! 6. Digitando nella barra di ricerca una stringa di caratteri, visualizzo i contatti il cui nome contiene tale stringa
+    $('#search-input').keyup(function() {
+        searchContact();
+    });
 });
 
 // ! *** VARIABILI GLOBALI ***
-var thisChat;
 var frasiRandom = [
     'Ciao!',
     'Ehi! Dimmi',
     'Bonjour',
     'No dai...',
-    'Che ti posso dire... sono d\'accordo'
+    'Che ti posso dire... sono pienamente d\'accordo. Tu sì che sai eh?!',
+    'Questo messaggio non ha alcun senso, è volutamente ideato affinché la sua lunghezza attivi il controllo sul troncamento dell\'anteprima nei contatti a sinistra'
 ];
 
 // *** FUNZIONI *** //
@@ -56,18 +60,18 @@ function chat() {
     if (testo.trim()) {
         $('#text-input').val('');
         // Memorizzo l'indice della chat in cui invierò il messaggio (per evitare che, cliccando su un'altra chat prima della ricezione del messaggio, questo venga spedito alla chat 'active' appena selezionata)
-        thisChat = $('.history-chat.active').index();
+        var thisChat = $('.history-chat.active').index();
         // Invoco la funzione 'invia' per generare un messaggio 'sent' e cambio la visualizzazione del tasto 'send' con 'mic'
-        invia(testo,'sent');
+        invia(testo,'sent',thisChat);
         $('#send-btn,#mic-btn').toggle();
         // Dopo 1 secondo la invoco nuovamente (con gli argomenti specifici per generare un messaggio 'received')
-        setTimeout(invia,1000,frasiRandom[numRandom(0,frasiRandom.length - 1)],'received');
+        setTimeout(invia,1000,frasiRandom[numRandom(0,frasiRandom.length - 1)],'received',thisChat);
         //
     }
 }
 
 // b. Gestione della generazione dei messaggi col metodo .clone() e dell'orario corrente, con successivo aggiornamento della scrollbar
-function invia(messaggio,classe) {
+function invia(messaggio,classe,indiceCorrente) {
     // Clonazione del template
     var copia = $('.template .message').clone();
     // Generazione orario corrente
@@ -81,10 +85,18 @@ function invia(messaggio,classe) {
         copia.addClass('sent');
     } else {
         copia.addClass('received');
+        $('.last-seen span').text(orario); // aggiornamento ultimo accesso //
     }
     // Incollo il clone al DOM
-    var originalChat = $('.history-chat').eq(thisChat);
+    var originalChat = $('.history-chat').eq(indiceCorrente);
     originalChat.append(copia);
+    // Controllo per il troncamento dei messaggi eccedenti un certo numero di caratteri
+    if (messaggio.length > 40) {
+        messaggio = messaggio.substring(0,40) + '...';
+    }
+    // Aggiornamento di anteprima messaggio e orario nella lista contatti
+    $('.contact-box').eq(indiceCorrente).find('.contact-message').text(messaggio);
+    $('.contact-box').eq(indiceCorrente).find('.last-time').text(orario);
     // Aggiornamento scrollbar (col metodo prop() di jQuery 1.6)
     $(originalChat).animate({scrollTop: $(originalChat).prop('scrollHeight')},0);
     // $(originalChat).animate({scrollTop: $(originalChat)[0].scrollHeight},0) funziona, è di jQuery 1.5. $('#main').animate({scrollTop: $(copia).offset().top},0) non funzionava, metodo offset() è impreciso nel calcolo dell'altezza
@@ -101,6 +113,28 @@ function addZero(num) {
 // d. Generazione numeri random tra min e max compresi
 function numRandom(min,max) {
     return Math.floor(Math.random()*(max - min + 1) + min);
+}
+
+// e. Ricerca di un contatto
+function searchContact() {
+    var filtrato = $('#search-input').val().toLowerCase();
+    var contatti = $('.contact-box');
+
+    for (var i = 0; i < contatti.length; i++) {
+        var elemento = contatti.eq(i).find('.contact-name').text();
+        if (elemento.toLowerCase().includes(filtrato)) {
+            contatti.eq(i).show();
+        } else {
+            contatti.eq(i).hide();
+        }
+    }
+
+    // Se non ci sono contatti corrispondenti, visualizzo apposito messaggio
+    if ($('.contact-box:visible').length == 0) {
+        $('#zero-found').show().text('Nessun contatto trovato');
+    } else {
+        $('#zero-found').hide();
+    }
 }
 
 // $(document).ready(function() {
